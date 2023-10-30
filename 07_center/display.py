ad5b60570443d40657099bba531d83c180ccd3bb
Greg Wilson <gvwilson@third-bit.com>
2023-10-30 16:20:59 -0400

    fix: simplifying data display script

--- 07_center/display.py
+++ 07_center/display.py
@@ -14,33 +14,37 @@ FIG_SIZE = 800
 def main():
     """Main driver."""
     args = parse_args()
+
     con = sqlite3.connect(args.dbfile)
     sites = pd.read_sql("select * from sites", con)
     surveys = pd.read_sql("select * from surveys", con)
     samples = pd.read_sql("select * from samples", con)
 
     combined = sites\
-        .set_index("site")\
-        .join(surveys.set_index("site"), how="inner")\
-        .reset_index()\
+        .merge(surveys, how="inner", on="site")\
         .drop(["lon", "lat"], axis=1)\
-        .set_index("label")\
-        .join(samples.set_index("label"), how="inner")\
-        .reset_index()
+        .merge(samples, how="inner", on="label")
 
-    for (i, site) in enumerate(sites["site"]):
-        temp = combined[combined["site"] == site]
-        center_lon = sum(temp["lon"] * temp["reading"]) / sum(temp["reading"])
-        center_lat = sum(temp["lat"] * temp["reading"]) / sum(temp["reading"])
-        title = f"{site}: lon={center_lon:.5f} lat={center_lat:.5f}"
-        fig = px.scatter(x=temp["lon"], y=temp["lat"], size=temp["reading"], title=title)
-        fig.add_vline(x=center_lon)
-        fig.add_hline(y=center_lat)
-        fig.update_layout(width=FIG_SIZE, height=FIG_SIZE)
-        if args.figdir:
-            fig.write_image(f"{args.figdir}/{site}.svg")
-        else:
-            fig.show()
+    for site in sites["site"]:
+        rows = combined[combined["site"] == site]
+        create_figure(args, site, rows)
+
+
+def create_figure(args, site, values):
+    """Create figure for particular site."""
+    center_lon = sum(values["lon"] * values["reading"]) / sum(values["reading"])
+    center_lat = sum(values["lat"] * values["reading"]) / sum(values["reading"])
+
+    title = f"{site}: lon={center_lon:.5f} lat={center_lat:.5f}"
+    fig = px.scatter(x=values["lon"], y=values["lat"], size=values["reading"], title=title)
+    fig.add_vline(x=center_lon)
+    fig.add_hline(y=center_lat)
+    fig.update_layout(width=FIG_SIZE, height=FIG_SIZE)
+
+    if args.figdir:
+        fig.write_image(f"{args.figdir}/{site}.svg")
+    else:
+        fig.show()
 
 
 def parse_args():
