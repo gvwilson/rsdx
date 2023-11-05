2e1fceaf6f2501e3389834f160dc3339348c83c2
Greg Wilson <gvwilson@third-bit.com>
2023-11-04 20:00:43 -0400

    feat: write first tests for grid class

--- src/test/grid.py
+++ src/test/grid.py
@@ -1,6 +1,5 @@
 """Represent 2D grid."""
 
-import random
 import sys
 
 
@@ -10,7 +9,7 @@ class Grid:
     UNINIT = -1
     FILLED = 0
 
-    def __init__(self, width, height, depth):
+    def __init__(self, width, height, depth, values=None):
         """Construct and fill."""
         self._width = width
         self._height = height
@@ -19,6 +18,8 @@ class Grid:
             [self.UNINIT] * self._height
             for _ in range(self._width)
         ]
+        if values is not None:
+            self.overwrite(values)
 
     def width(self):
         """Get width of grid."""
@@ -45,17 +46,17 @@ class Grid:
         x, y = key
         assert 0 <= x < self._width
         assert 0 <= y < self._height
-        assert 0 <= value <= self._depth
+        assert (value == Grid.FILLED) or (1 <= value <= self._depth)
         self._grid[x][y] = value
 
     def __eq__(self, other):
         """Compare to another grid."""
-        if self.width() != other.width():
+        if self._width != other.width():
             return False
-        if self.height() != other.height():
+        if self._height != other.height():
             return False
-        for x in range(self.width()):
-            for y in range(self.height()):
+        for x in range(self._width):
+            for y in range(self._height):
                 if self[x, y] != other[x, y]:
                     return False
         return True
@@ -63,33 +64,39 @@ class Grid:
     def adjacent(self, x, y):
         """Is (x, y) adjacent to a filled cell?"""
         x_1, y_1 = x + 1, y + 1
-        if (x > 0) and (self[x - 1, y] == FILLED):
+        if (x > 0) and (self[x - 1, y] == self.FILLED):
             return True
-        if (x_1 < self.width()) and (self[x_1, y] == FILLED):
+        if (x_1 < self._width) and (self[x_1, y] == self.FILLED):
             return True
-        if (y > 0) and (self[x, y - 1] == FILLED):
+        if (y > 0) and (self[x, y - 1] == self.FILLED):
             return True
-        if (y_1 < self.height()) and (self[x, y_1] == FILLED):
+        if (y_1 < self._height) and (self[x, y_1] == self.FILLED):
             return True
         return False
 
     def on_border(self, x, y):
         """Is this cell on the border of the grid?"""
-        if (x == 0) or (x == self.width() - 1):
+        if (x == 0) or (x == self._width - 1):
             return True
-        if (y == 0) or (y == self.height() - 1):
+        if (y == 0) or (y == self._height - 1):
             return True
         return False
 
     def print(self, stream=sys.stdout, details="full"):
         """Show the result."""
-        print(self.width(), self.height(), self.depth(), file=stream)
+        print(self._width, self._height, self._depth, file=stream)
         if details == "brief":
             return
-        for y in range(self.height() - 1, -1, -1):
-            for x in range(self.width()):
+        for y in range(self._height - 1, -1, -1):
+            for x in range(self._width):
                 if details == "numbers":
                     stream.write(f"{self[x, y]:02d} ")
                 else:
-                    stream.write("X" if self[x, y] == FILLED else ".")
+                    stream.write("X" if self[x, y] == self.FILLED else ".")
             stream.write("\n")
+
+    def overwrite(self, values):
+        """Overwrite with values."""
+        assert len(values) == self._width
+        assert all(len(v) == self._height for v in values)
+        self._grid = [v[:] for v in values]
