3b9bf90823803bcee8bf6b88dce394a4cc7d517d
Greg Wilson <gvwilson@third-bit.com>
2023-11-14 19:57:14 -0500

    feat: prev/next navigation links

--- lib/book/extensions/filters.py
+++ lib/book/extensions/filters.py
@@ -17,6 +17,18 @@ def is_root(node):
     return len(node.path) == 0
 
 
+@ibis.filters.register("link_next")
+def link_next(node):
+    """Create next-page link."""
+    return _link_nav(node, "next")
+
+
+@ibis.filters.register("link_prev")
+def link_prev(node):
+    """Create previous-page link."""
+    return _link_nav(node, "prev")
+
+
 @ibis.filters.register("not_root")
 def not_root(node):
     """Is this _not_ the root node?"""
@@ -46,3 +58,24 @@ def syllabus(node):
         [f"<li>{util.markdownify(s, True)}</li>" for s in meta["syllabus"]]
     )
     return f'<ul class="syllabus">\n{items}\n</ul>'
+
+
+def _link_nav(node, kind):
+    """Generate previous/next page links."""
+    if not node.slug:
+        return ""
+    chapters = ark.site.config["chapters"]
+    try:
+        where = chapters.index(node.slug)
+    except ValueError:
+        util.fail(f"slug {node.slug} not found in chapters")
+    if kind == "prev":
+        if where == 0:
+            return ""
+        return f'<a href="@root/{chapters[where - 1]}/">&lArr;</a>'
+    elif kind == "next":
+        if where == (len(chapters) - 1):
+            return ""
+        return f'<a href="@root/{chapters[where + 1]}/">&rArr;</a>'
+    else:
+        util.fail(f"Unknown nav link type '{kind}'")
